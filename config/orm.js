8172494a74fc36e4fbd8e81fb46eee4d4597f73c
jsp <jonsp14@gmail.com>
2020-10-21 21:50:25 -0700

    update orm, model

--- config/orm.js
+++ config/orm.js
@@ -1,16 +1,48 @@
-const connection = require("./connection")
+const connection = require("./connection.js")
 
-const obj = {
-    selectAll: function () {
-        connection.query("SELECT * FROM burgers");
+function printQuestionMarks(num) {
+    var arr = [];
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+    return arr.toString();
+}
+
+const orm = {
+    selectAll: function (tableInput, cb) {
+        var queryString = "SELECT * FROM ??";
+        connection.query(queryString), [tableInput], function (err, result) {
+            if (err) throw err;
+            console.log(result);
+            cb(result);
+        }
     },
-    insertOne: function (x) {
-        connection.query(`INSERT INTO burgers SET burger_name = ${x}`);
+
+    insertOne: function (tableInput, col, cb) {
+        var queryString = "INSERT INTO " + tableInput;
+
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        connection.query(queryString), [tableInput, col], function (err, result) {
+            if (err) throw err;
+            console.log(result);
+            cb(result);
+        }
     },
-    updateOne: function (x) {
-        connection.query(`UPDATE burger SET devoured = true WHERE burger_name = ${x}`);
+    updateOne: function (tableInput, col1, trueFalse, col2, value, cb) {
+        var queryString = "UPDATE ?? SET ?? = ? WHERE ?? = ?";
+        connection.query(queryString), [tableInput, col1, trueFalse, col2, value], function (err, result) {
+            if (err) throw err;
+            console.log(result);
+            cb(result);
+        }
     }
 };
 
 
-module.exports = obj;
+module.exports = orm;
--- models/burger.js
+++ models/burger.js
@@ -1,7 +1,25 @@
-const orm = require("obj");
+const orm = require("../config/orm.js");
 
 // code that will call ORM functions using burger specific input
 // export at the end of file
+
+var burger = {
+    selectAll: function(cb) {
+        orm.selectAll("burgers", function(res){
+            cb(res);
+        });
+    }, 
+    insertOne: function(col, cb){
+        orm.insertOne("burgers", col, function(res){
+            cb(res);
+        });
+    },
+    updateOne: function(col1, trueFalse, col2, value, cb){
+        orm.updateOne("burgers", col1, trueFalse, col2, value, function(res){
+            cb(res);
+        });
+    }
+}
 
 
 
